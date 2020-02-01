4db20a37d0cbd7168240a816faac747dcd999fbb
Emily <bruner.emilym@gmail.com>
2020-02-01 12:04:18 -0500

    made seeds

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,13 +1,9 @@
 
 exports.seed = function(knex) {
 
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: "test@test.com", password: "pass"},
-        {id: 2, username: 'test2@test.com', password: "pass"}
+        {username: "demo@test.com", password: "pass"},
+        {username: 'demo2@test.com', password: "pass2"}
        
       ]);
-    });
 };
--- database/seeds/003-projects.js
+++ database/seeds/003-projects.js
@@ -5,9 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
 
       return knex('projects').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, student_id: 1, title: "Recommendation Due", due_date: "02-13-2020", reminder_time: "3:00", notes: "Must write recommendation for masters program"},
+        {id: 2, student_id: 2, title: "Comments on Research Paper Due", due_date: "02-23-2020", reminder_time: "4:00", notes: "Must return comments and edits for midterm paper"},
+        {id: 3, student_id: 2, title: "Give feedback on mock interview", due_date: "02-28-2020", reminder_time: "12:00", notes: "Give feedback for mock interview excercise"},
+        {id: 4, student_id: 3, title: "Letter of Recommendation", due_date: "03-13-2020", reminder_time: "1:00", notes: "Must write recommendation for job application"},
+        {id: 5, student_id: 4, title: "Due from Student", due_date: "02-10-2020", reminder_time: "7:00", notes: "Remind student that research paper is due"},
       ]);
     });
 };
