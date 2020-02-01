9f45ce07e14fc31c5aa925934daa064172605e64
Emily <bruner.emilym@gmail.com>
2020-01-31 21:02:42 -0500

    created initial table set up and made first migration

--- database/migrations/20200131200634_bpapp.js
+++ database/migrations/20200131200634_bpapp.js
@@ -1,8 +1,42 @@
-
-exports.up = function(knex) {
+exports.up = async function(knex) {
+    await knex.schema.createTable("users", (table) => {
+        table.increments("id").unique()
+        table.string("email").unique().notNullable()
+        table.string("password").notNullable()
   
-};
-
-exports.down = function(knex) {
+    })
   
-};
+    await knex.schema.createTable("students", (table) => {
+      table.increments("id").unique()
+      table.integer("professor_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      table.string("name").notNullable()
+      table.string("email").notNullable()
+      table.string("image_url")
+  
+  })
+  
+    await knex.schema.createTable("projects", (table) => {
+        table.increments("id").unique()
+        table.integer("student_id")
+        .notNullable()
+        .references("id")
+        .inTable("students")
+        table.date("due_date")
+        table.time("reminder_time")
+        table.string("notes")
+  
+    })
+  
+    
+  };
+  
+  exports.down = async function(knex) {
+      await knex.schema.dropTableIfExists("recipe_ingredients")
+      await knex.schema.dropTableIfExists("ingredients")
+      await knex.schema.dropTableIfExists("recipes")
+  
+  };
+  
